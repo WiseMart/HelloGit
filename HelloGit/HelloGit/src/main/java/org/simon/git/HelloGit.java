5a8902b35ad652ca986429805b93f743cc8ca9c5
simon.pro <simonli@google.com>
2025-02-17 11:50:46 -0800

    first commit

---
+++ HelloGit/HelloGit/src/main/java/org/simon/git/HelloGit.java
@@ -0,0 +1,15 @@
+package org.simon.git;
+
+/**
+ * Classname: HelloGit
+ * Package: org.simon.git
+ * Description:
+ * Author: Simon Li
+ * Create: 2/17/2025 -11:36 AM
+ * Version: v1.0
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!!");
+    }
+}
---
+++ HelloGit/HelloGit/src/main/java/org/simon/git/Main.java
@@ -0,0 +1,15 @@
+package org.simon.git;
+
+/**
+ * Classname: ${NAME}
+ * Package: org.simon.git
+ * Description:
+ * Author: Simon Li
+ * Create: ${DATE} -${TIME}
+ * Version: v1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
