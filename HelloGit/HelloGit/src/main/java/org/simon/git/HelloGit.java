4f5e69d05d7c110d6b3e31673221a97c7f538efc
simon.pro <simonli@google.com>
2025-02-17 13:28:23 -0800

    create new branch

--- HelloGit/HelloGit/src/main/java/org/simon/git/HelloGit.java
+++ HelloGit/HelloGit/src/main/java/org/simon/git/HelloGit.java
@@ -12,5 +12,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!!");
         System.out.println("Hello Git!! Version1");
+        System.out.println("Hello Git!! create new branch");
     }
 }
